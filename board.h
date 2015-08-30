068076523f1a73cd74608b2a15c387184bdc28cf
Ken Fox <ken.fox@atomicobject.com>
2015-08-30 14:29:47 -0400

    make Board::Grid type public

--- board.h
+++ board.h
@@ -50,18 +50,19 @@ public:
     void print();
     Board &setup(int x, int y, Status status);
 
-private:
     using Grid = unsigned long long;
+
+    static Grid bit(int x, int y) {
+        return 1L << (63 - (y * 8 + x));
+    }
+
+private:
 
     Grid taken; // bit set when a square is taken
     Grid owner; // bit set when owner is black
 
     static Grid flip(Grid taken, Grid owner, bool toBlack,
                      int x, int y, int dx, int dy);
-
-    static Grid bit(int x, int y) {
-        return 1L << (63 - (y * 8 + x));
-    }
 };
 
 #endif
